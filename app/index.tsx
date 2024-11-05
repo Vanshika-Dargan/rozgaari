04c5366e2e5262a317e879abc8c4ffacbc7cdb49
Vanshika-Dargan <vanshikadargan.vd@gmail.com>
2024-11-05 14:58:53 +0530

    added the login page

--- app/index.tsx
+++ app/index.tsx
@@ -1,187 +1,133 @@
-import React, { useState } from "react";
-import { Text, View, Pressable, StyleSheet, Image } from "react-native";
+import React from 'react';
+import { View, Text, TextInput, StyleSheet, Pressable, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import { LinearGradient } from "expo-linear-gradient";
 import MaskedView from "@react-native-masked-view/masked-view";
 
-export default function Index() {
-  const [selectedLanguage, setSelectedLanguage] = useState("en");
-  const [progress, setProgress] = useState(0.1);
-
+const PhoneNumberInput = () => {
   return (
-    <View style={styles.container}>
-      {/* Progress Bar */}
-      {/* <View style={styles.progressContainer}>
-        <View style={[styles.progressBar, { width: `${progress * 100}%` }]} />
-      </View> */}
-
-      
-      <View style={styles.lang}>
-        <Text style={styles.title}>{selectedLanguage==="en" ? "Select Language" : "भाषा चुनें"}</Text>
-
-        <View style={styles.buttonContainer}>
-          <LinearGradient
-            colors={["#32a89f", "#1b6b8a"]}
-            style={styles.gradientBorder}
-          >
-            <Pressable
-              style={[
-                styles.button,
-                selectedLanguage === "en" && styles.buttonSelected,
-              ]}
-              onPress={() => {
-                setSelectedLanguage("en");
-                setProgress(0.2);
-              }}
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={styles.bottomBox}>
+        <View style={styles.container}>
+          <View style={styles.loginContainer}>
+            <Text style={styles.heading}>Log in or sign up</Text>
+          </View>
+          <View style={styles.inputContainer}>
+            <TextInput
+              style={styles.countryCodeInput}
+              keyboardType="phone-pad"
+              defaultValue="+91"
+            />
+            <View style={styles.verticalBar} />
+            <TextInput
+              style={styles.phoneNumberInput}
+              placeholder="Enter Phone Number"
+              placeholderTextColor="#888"
+              keyboardType="phone-pad"
+            />
+          </View>
+          <View style={styles.bottomContainer}>
+            <LinearGradient
+              colors={["#ff5e3a", "#ff2a00"]}
+              style={[styles.gradientFilledButton]}
             >
-              <MaskedView maskElement={<Text style={styles.buttonTextGradient}>English</Text>}>
-              <LinearGradient  colors={["#32a89f", "#1b6b8a"]}>
-              <Text style={styles.buttonText}>English</Text>
-              </LinearGradient>
-              </MaskedView>
-              
-             
-            </Pressable>
-          </LinearGradient>
-
-          <LinearGradient
-            colors={["#32a89f", "#1b6b8a"]}
-            style={styles.gradientBorder}
-          >
-            <Pressable
-              style={[
-                styles.button,
-                selectedLanguage === "hi" && styles.buttonSelected,
-              ]}
-              onPress={() => {
-                setSelectedLanguage("hi");
-                setProgress(0.2);
-              }}
-            >
-             <MaskedView maskElement={<Text style={styles.buttonTextGradient}>हिंदी</Text>}>
-              <LinearGradient  colors={["#32a89f", "#1b6b8a"]}>
-              <Text style={styles.buttonText}>हिंदी</Text>
-              </LinearGradient>
-              </MaskedView>
-            </Pressable>
-          </LinearGradient>
+              <Text style={styles.buttonText}>Continue</Text>
+            </LinearGradient>
+          </View>
+          <View style={styles.tncContainer}>
+            <Text style={styles.heading3}>
+              By continuing you agree to Rozgaari 
+            </Text>
+            <View style={styles.container2}>
+              <Text style={styles.tnc}>Terms of Use </Text>
+              <Text>and </Text>
+              <Text style={styles.tnc}>Privacy Policy</Text>
+            </View>
+          </View>
         </View>
       </View>
-      
-      {/* <View style={styles.bottomContainer}>
-      <LinearGradient
-            colors={["#ff5e3a", "#ff2a00"]} 
-            style={styles.gradientBorderContinue}
-          >
-            <View
-              style={[
-                styles.continue
-              ]}
-            >
-             <MaskedView maskElement={<Text style={styles.buttonTextGradient}>&gt;</Text>}>
-              <LinearGradient  colors={["#ff5e3a", "#ff2a00"]} >
-              <Text style={styles.buttonText}>&gt;</Text>
-              </LinearGradient>
-              </MaskedView>
-            </View>
-          </LinearGradient>
-      </View> */}
-    </View>
+    </TouchableWithoutFeedback>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
+  container2: {
+    flexDirection: 'row',
+    columnGap: 8
+  },
+  heading3: {},
+  tnc: {
+    fontSize: 13,
+    color: '#888',
+  },
+  tncContainer: {
+    width: 300,
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    bottom: -220,
+    rowGap: 6
+  },
+  gradientFilledButton: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 300,
+    borderRadius: 10,
+    backgroundColor: "white",
+    paddingHorizontal: 10,
+    paddingVertical: 15,
+  },
+  heading: {
+    fontSize: 18,
+    fontWeight: 'bold'
+  },
+  verticalBar: {
+    width: 1,
+    height: '150%',
+    backgroundColor: '#ccc',
+    marginLeft: 20,
+    marginRight: 10
+  },
+  bottomBox: {
     flex: 1,
-    alignItems: "center",
-    backgroundColor: "black", // Black background color
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
   },
-  progressContainer: {
-    marginTop: 80,
-    height: 10,
-    width: "80%",
-    backgroundColor: "#e0e0e0",
-    borderRadius: 5,
-    overflow: "hidden",
-    marginBottom: 20,
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 30
   },
-  progressBar: {
-    height: "100%",
-    backgroundColor: "#4f83cc",
-  },
-  title: {
-    fontSize: 30,
-    fontWeight: "bold",
-    color: "white",
+  loginContainer: {
     marginBottom: 50,
   },
-  buttonContainer: {
-    flexDirection: "column",
-    width: "80%",
-    justifyContent: "center",
-    alignItems: "center",
-    rowGap: 10
-   
-  },
-  gradientBorder: {
-    padding: 3, // Border thickness
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderWidth: 2,
+    borderColor: 'blue',
+    paddingHorizontal: 10,
+    paddingVertical: 15,
+    width: 300,
     borderRadius: 10,
-    margin: 5,
-    width: "100%"
   },
-  gradientBorderContinue: {
-    padding: 3, // Border thickness
-    borderRadius: 30,
-    margin: 5,
+  countryCodeInput: {
+    fontSize: 18,
   },
-  button: {
-    width: "100%",
-    paddingVertical: 20,
-    paddingHorizontal: 30,
-    borderRadius: 10,
-    backgroundColor: "black", 
-    alignItems: "center",
-    color: "white",
-  },
-  continue: {
-    height: 40, 
-    width: 40, 
-    borderRadius: 20, 
-    backgroundColor: "black",
-    alignItems: "center",
-    justifyContent: "center", 
-  },
-  
-  buttonSelected: { 
-    backgroundColor: "#30363e"
+  phoneNumberInput: {
+    fontSize: 18,
   },
   buttonText: {
-    opacity: 0,
-    fontWeight: "bold",
-    fontSize: 18,
-  },
-  buttonTextGradient: {
-    backgroundColor: "transparent",
-    fontWeight: "bold",
-    fontSize: 18,
-  },
-  lang: {
-    flex: 1,
-    width: "100%",
-    alignItems: "center",
-    justifyContent: "center",
-   
+    color: '#FFFFFF',
+    fontSize: 16,
+    fontWeight: 'bold',
   },
   bottomContainer: {
-    width: "90%",
-    paddingVertical: 15,
-    marginBottom: 30,
+    marginTop: 70,
     justifyContent: "center",
-    alignItems: "flex-end",
+    alignItems: "center",
+    width: 300,
   },
-  bottomBtn: {
-    fontSize: 18,
-  },
-  map:{
-   
-  }
 });
+
+export default PhoneNumberInput;
